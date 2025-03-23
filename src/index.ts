7cc0dec13798b055ba2a7cf7122ae8feeae48b25
varun789-mx <varunsh153@gmail.com>
2025-03-23 16:50:58 +0000

    First commit

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,23 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+export interface Env { 
+}
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		const url = new URL(request.url);
-		switch (url.pathname) {
-			case '/message':
-				return new Response('Hello, World!');
-			case '/random':
-				return new Response(crypto.randomUUID());
-			default:
-				return new Response('Not Found', { status: 404 });
+export default { 
+	async fetch (request:Request,env:Env,ctx:ExectionContext):Promise<Response> {
+		console.log(request.body);
+		console.log(request.headers);
+		console.log(request.method); 
+
+		if (request.method === "GET") { 
+			return Response.json({
+				message:"We got a Get reqest"
+			});
 		}
+		else { 
+			return Response.json({
+				message:"Get request not received"
+			});
+		}
+
 	},
-} satisfies ExportedHandler<Env>;
+}
+
