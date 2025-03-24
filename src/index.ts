e088ad394a772ce768361961c9b060ae4de96e98
Varun789-mx <varunsh153@gmail.com>
2025-03-24 07:55:49 +0000

    Minor change

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,8 @@ export interface Env {
 }
 
 export default { 
-	async fetch (request:Request,env:Env,ctx:ExectionContext):Promise<Response> {
+	async fetch (request:Request,env:Env,ctx:ExecutionContext):Promise<Response> {
 		console.log(request.body);
-		console.log(request.headers);
-		console.log(request.method); 
 
 		if (request.method === "GET") { 
 			return Response.json({
